56b3c51376158e19602097805b496dd7f23474e1
Alberto Junior <albertogouvea809@gmail.com>
2023-12-06 23:27:12 -0300

    implemented the bomb check system. Now the game are playable.

--- main.c
+++ main.c
@@ -4,42 +4,85 @@
 #include <string.h>
 
 int main(){
-	int coluna = 1;
+	int coluna = 1;																									//DECLARAÇÔES{
 	char linha = 'd';
 	int bloco[4][4];
+	char tela[4][4] = {{'?', '?', '?', '?'}, {'?', '?', '?', '?'}, {'?', '?', '?', '?'}, {'?', '?', '?', '?'}};
 	int bomb;
 	char charLinha;
-	int intColuna, intLinha, charColuna;
+	int intColuna, intLinha, charColuna;																			//}
 
 
-	srand(time(NULL));                                 //GERACAO DAS BOMBAS{
+	srand(time(NULL));                                                   //GERACAO DAS BOMBAS{
 	for(int i = 0; i < 4; i++){
 		for(int j = 0; j < 4; j++){
 			bomb = rand() % 3 + 1;
 			if(bomb == 1){
-				bloco[i][j]++;
+				bloco[i][j] = -1;
 			}
 		}
-	}                                                   //}
+	}                                                                    //}
 
-	do{
+	for(int i = 0; i < 4; i++){																                    	//CHECAGEM DAS BOMBAS{
+		for(int j = 0; j < 4; j++){
+			if(bloco[i][j] != -1){
+				if(i - 1 >= 0 && i - 1 < 4 && j >= 0 && j < 4 && bloco[i - 1][j] == -1){
+					bloco[i][j]++;
+				}
+				if(i - 1 >= 0 && i - 1 < 4 && j + 1 >= 0 && j + 1 < 4 && bloco[i - 1][j + 1] == -1){
+					bloco[i][j]++;
+				}
+				if(i >= 0 && i < 4 && j + 1 >= 0 && j + 1 < 4 && bloco[i][j + 1] == -1){
+					bloco[i][j]++;
+				}
+				if(i + 1 >= 0 && i + 1 < 4 && j + 1 >= 0 && j + 1 < 4 && bloco[i + 1][j + 1] == -1){
+					bloco[i][j]++;
+				}
+				if(i + 1 >= 0 && i + 1 < 4 && j >= 0 && j < 4 && bloco[i + 1][j] == -1){
+					bloco[i][j]++;
+				}
+				if(i + 1 >= 0 && i + 1 < 4 && j - 1 >= 0 && j - 1 < 4 && bloco[i + 1][j - 1] == -1){
+					bloco[i][j]++;
+				}
+				if(i >= 0 && i < 4 && j - 1 >= 0 && j - 1 < 4 && bloco[i][j - 1] == -1){
+					bloco[i][j]++;
+				}
+				if(i - 1 >= 0 && i - 1 < 4 && j - 1 >= 0 && j - 1 < 4 && bloco[i - 1][j - 1] == -1){
+					bloco[i][j]++;
+				}
+			}
+		}
+	}																								               		//}
 
-		for(int i = 0; i < 4; i++){                          //GERACAO DA TELA{
-			printf("%c   ", linha);
+	intLinha = -10;
+	intColuna = -10;
+
+	do{														              //GERACAO DA TELA{
+
+		for(int i = 0; i < 4; i++){
+			printf("%c  ", linha);
 			linha--;
 			for(int j = 0; j < 4; j++){
-				printf("%i ", bloco[i][j]);
+				if(i == intLinha && j == intColuna){
+					printf("%i ", bloco[i][j]);
+					tela[i][j] = '0' + bloco[i][j];
+				}else{
+					printf("%c ", tela[i][j]);
+				}
 			}
 			printf("\n");
 		}
-		printf("\n    ");
+
+
+		printf("\n   ");
 		for(int i = 0; i < 4; i++){
 			printf("%i ", coluna);
 			coluna++;
-		}                                                     //}
+		}                                                                 //}
 
 
-		printf("\n");                                                    //ADQUIRIR LOCAL DA MATRIZ{
+		printf("\n");                                                      //ADQUIRIR LOCAL DA MATRIZ{
+		do{
 			printf("Digite a linha: (a - b - c - d)\n");
 			scanf(" %c", &charLinha);
 				switch(charLinha){
@@ -55,8 +98,12 @@ int main(){
 				case 'd':
 					intLinha = 0;
 					break;
+				default:
+					printf("Linha inválida. Tente novamente.\n");
 				}
+		}while(charLinha < 'a' || charLinha > 'd');
 
+		do{
 			printf("Digite a coluna: (1 - 2 - 3 - 4)\n");
 			scanf("%i", &charColuna);
 				switch(charColuna){
@@ -72,18 +119,19 @@ int main(){
 				case 4:
 					intColuna = 3;
 					break;
+				default:
+					printf("Coluna inválida. Tente novamente.\n");
 				}
-
-		if(bloco[intLinha][intColuna] != 1){
-			printf("Ufa! Não era uma bomba!\n\n");
-		}
+		}while(charColuna < 1 || charColuna > 4);								//}
 
 		coluna = 1;
 		linha = 'd';
 
-	}while(bloco[intLinha][intColuna] != 1);
+	}while(bloco[intLinha][intColuna] != -1);
 
 	printf("Que pena! Você perdeu :(");
 
 	return 0;
 }
+
+
